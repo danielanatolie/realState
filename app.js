02d8a83abda5c694b88b81ec8d535d40bf57b168
ridentcode <daniel.anatolie@gmail.com>
2017-08-18 19:20:34 +0000

    Visual improvements with bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+    var properties = [
+            {name: "Surrey Night Castle", image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/BlaiseCastleEstate%28Castle%29.JPG"},
+            {name: "North Beach Hall", image: "http://www.publicdomainpictures.net/pictures/140000/velka/house-on-the-beach.jpg"},
+            {name: "York House", image: "https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg"}
+        ] 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
@@ -8,12 +16,20 @@ app.get("/", function(req, res) {
 });
 
 app.get("/properties", function(req, res) {
-    var properties = [
-            {name: "Surrey Night Castle", image: "https://upload.wikimedia.org/wikipedia/commons/7/7b/BlaiseCastleEstate%28Castle%29.JPG"},
-            {name: "North Beach Hall", image: "http://www.publicdomainpictures.net/pictures/140000/velka/house-on-the-beach.jpg"},
-            {name: "York House", image: "https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg"}
-        ] 
+
     res.render("properties", {properties: properties});
+});
+
+app.post("/properties", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image; 
+    var newProperty = {name: name, image: image};
+    properties.push(newProperty);
+    res.redirect("/properties");
+});
+
+app.get("/properties/new", function(req, res) {
+   res.render("new.ejs"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
