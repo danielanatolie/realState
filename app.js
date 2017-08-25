a9002109c97b76f51329baf1e0f5a592b8d05f16
ridentcode <daniel.anatolie@gmail.com>
2017-08-25 05:28:26 +0000

    Authentication System

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    Property   = require("./models/property"),
-    Comment    = require("./models/comment"),
-    seedDB     = require("./seeds");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    Property        = require("./models/property"),
+    Comment         = require("./models/comment"),
+    seedDB          = require("./seeds"),
+    passport        = require("passport"),
+    localStrategy   = require("passport-local"),
+    User            = require("./models/user");
+    
 
 mongoose.connect("mongodb://localhost/realState");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+app.use(require("express-session")({
+    secret: "Descipline and Consistency.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -54,7 +74,7 @@ app.get("/properties/:id", function(req, res) {
     });
 });
 
-app.get("/properties/:id/comments/new", function(req, res) {
+app.get("/properties/:id/comments/new", isLoggedIn, function(req, res) {
     Property.findById(req.params.id, function(err, property) {
        if(err) {
            console.log(err);
@@ -64,7 +84,7 @@ app.get("/properties/:id/comments/new", function(req, res) {
     });
 });
 
-app.post("/properties/:id/comments", function(req, res) {
+app.post("/properties/:id/comments", isLoggedIn, function(req, res) {
     Property.findById(req.params.id, function(err, property) {
        if(err) {
            console.log(err);
@@ -82,6 +102,46 @@ app.post("/properties/:id/comments", function(req, res) {
        }
     });
 });
+
+app.get("/register", function(req,res) {
+   res.render("register");
+});
+
+app.post("/register", function(req, res) {
+   var newUser = new User({username: req.body.username});
+   User.register(newUser, req.body.password, function(err, user) {
+      if(err) {
+          console.log(err);
+          return res.render("register");
+      } 
+      passport.authenticate("local")(req, res, function() {
+         res.redirect("/properties"); 
+      });
+   });
+});
+
+app.get("/login", function(req, res) {
+   res.render("login"); 
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/properties",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/properties");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    } 
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The realState server has started.");
