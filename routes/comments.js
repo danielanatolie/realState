1c9c442a1fda9a28b55ba7d55939e318d5f73eb7
ridentcode <daniel.anatolie@gmail.com>
2017-08-26 19:19:03 +0000

    destroy comment

--- routes/comments.js
+++ routes/comments.js
@@ -59,6 +59,17 @@ router.put("/:comment_id", function(req, res) {
     });
 });
 
+// destroy comment 
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+       if(err) {
+           res.redirect("back");
+       } else {
+           res.redirect("/properties/"+req.params.id);
+       }
+    });
+});
+
 // middleware 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
