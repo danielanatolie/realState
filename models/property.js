7f92eaa530bef4f1039257d74b4315bacd318cbd
ridentcode <daniel.anatolie@gmail.com>
2017-08-27 19:02:32 +0000

    landing page & dynamic pricing

--- models/property.js
+++ models/property.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 
 var propertySchema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author: {
--- routes/properties.js
+++ routes/properties.js
@@ -18,12 +18,13 @@ router.get("/", function(req, res) {
 router.post("/", middleware.isLoggedIn, function(req, res) {
     var name = req.body.name;
     var image = req.body.image; 
+    var price = req.body.price;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newProperty = {name: name, image: image, description: desc, author: author};
+    var newProperty = {name: name, price: price, image: image, description: desc, author: author};
     Property.create(newProperty, function(err, newlyCreated) {
         if(err) {
             console.log(err);
